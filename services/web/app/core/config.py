33fcc4e676f151b0b92951825f26a1117190560c
Ahmed Eltaher <ahmedheltaher@gmail.com>
2023-03-10 14:53:48 +0200

    Dwlete Images Fixed

--- services/web/app/core/config.py
+++ services/web/app/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     PROJECT_VERSION: Optional[str] = "1.0.0"
     PROJECT_ENVIRONMENT: str
 
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[str] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
